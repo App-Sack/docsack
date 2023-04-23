3b453173ed04faae1ad3dd576ba762a394be636d
chandansgowda <chandansuresh007@gmail.com>
2023-04-23 21:06:53 +0530

    added AppFile model

---
+++ lib/models/appfile.dart
@@ -0,0 +1,14 @@
+class AppFile{
+  String id;
+  String path;
+  String name;
+  String description;
+
+  AppFile({
+    required this.id,
+    required this.path,
+    required this.name,
+    required this.description,
+});
+
+}
